5f3bd91dd7da8bd23c6525f5a3e6d9e5e6b9dbcf
MohamadReslan <115694912+MohamadReslan@users.noreply.github.com>
2023-09-14 09:23:39 +0200

    pass other tests + add description for all routes in the swagger

--- Duncan/Controllers/SystemsController.cs
+++ Duncan/Controllers/SystemsController.cs
@@ -1,13 +1,14 @@
-using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Shard.Shared.Core;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Duncan.Controllers
 {
     public class SystemsController : ControllerBase
     {
+        [SwaggerOperation(Summary = "Get all systems with their planets")]
         [HttpGet("/systems")]
-
         public IReadOnlyList<SystemSpecification> GetSystems()
         {
             MapGeneratorOptions mapOptions = new MapGeneratorOptions();
@@ -17,15 +18,45 @@ namespace Duncan.Controllers
             return systems.Systems;
         }
 
+        [SwaggerOperation(Summary = "Get a single system, and all its planet")]
         [HttpGet("/systems/{systemName}")]
+
         public SystemSpecification GetOneSystem(string systemName)
         {
             MapGeneratorOptions mapOptions = new MapGeneratorOptions();
             mapOptions.Seed = "MohaMax";
             MapGenerator map = new MapGenerator(mapOptions);
             SectorSpecification systems = map.Generate();
-            IReadOnlyList <SystemSpecification> filteredSystem = systems.Systems.Where(system => system.Name == systemName).ToList();
+            IReadOnlyList<SystemSpecification> filteredSystem = systems.Systems.Where(system => system.Name == systemName).ToList();
             return filteredSystem[0];
+        }
+
+        [SwaggerOperation(Summary = "Get all planets of a single system")]
+        [HttpGet("/systems/{systemName}/planets")]
+
+        public IReadOnlyList<PlanetSpecification> GetPlanetsOfOneSystem(string systemName)
+        {
+            MapGeneratorOptions mapOptions = new MapGeneratorOptions();
+            mapOptions.Seed = "MohaMax";
+            MapGenerator map = new MapGenerator(mapOptions);
+            SectorSpecification systems = map.Generate();
+            IReadOnlyList<SystemSpecification> filteredSystem = systems.Systems.Where(system => system.Name == systemName).ToList();
+
+            return filteredSystem[0].Planets;
+        }
+
+        [SwaggerOperation(Summary = "Get a single planet of a system")]
+        [HttpGet("/systems/{systemName}/planets/{planetName}")]
+
+        public PlanetSpecification GetOnePlanet(string systemName,string planetName)
+        {
+            MapGeneratorOptions mapOptions = new MapGeneratorOptions();
+            mapOptions.Seed = "MohaMax";
+            MapGenerator map = new MapGenerator(mapOptions);
+            SectorSpecification systems = map.Generate();
+            IReadOnlyList<SystemSpecification> filteredSystem = systems.Systems.Where(system => system.Name == systemName).ToList();
+            PlanetSpecification respone = filteredSystem[0].Planets.Where(planet => planet.Name == planetName).ToArray()[0];
+            return respone;
         }
     }
 }
